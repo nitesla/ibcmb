c09692c2980d09803d15e762be2740b74e81b931
Oluwawunmi <soluwawunmi@gmail.com>
2017-03-24 17:47:33 +0100

    Basic Plain Project

---
+++ src/main/java/longbridge/InternetbankingApplication.java
@@ -0,0 +1,12 @@
+package longbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetbankingApplication.class, args);
+	}
+}
