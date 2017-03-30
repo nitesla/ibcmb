21972ffea0b84236dd74742d57bac1d69c5b614a
Yemi Dalley <Yemi Dalley@192.168.1.3>
2017-03-30 14:12:32 +0100

    Completed Code

--- src/main/java/longbridge/models/Code.java
+++ src/main/java/longbridge/models/Code.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 
 /**
  * Created by Fortune on 3/29/2017.
+ * 
+ * Code is lookup table (or enumeration) predicate
  */
 
 @Entity
@@ -13,34 +15,42 @@ public class Code extends AbstractEntity {
     private String type;
     private String description;
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Code other = (Code) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
 
-    @Override
+    public String getCode() {
+		return code;
+	}
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	@Override
     public String toString() {
         return "Code [id=" + id + ", code=" + code + ", description="
                 + description + ", type=" + type + "]";
     }
 
+    
 }
