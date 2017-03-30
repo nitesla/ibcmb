63dd3397b17234a24b1065af70eb3e976813aece
Farooq Ayoade <farooq.ayoade@longbridgetech.com>
2017-03-30 12:01:10 +0100

    Made modifications to the User models
    Added mappings for user in beneficiary models

---
+++ src/main/java/longbridge/repositories/UserRepo.java
@@ -0,0 +1,16 @@
+package longbridge.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+/**
+ * Created by Wunmi on 27/03/2017.
+ */
+@Repository
+public interface UserRepo<T,ID  extends Serializable> extends JpaRepository<T, ID> {
+
+    //User findByEmail(String email);
+
+}
---
+++ src/main/java/longbridge/services/UserService.java
@@ -0,0 +1,21 @@
+package longbridge.services;
+
+import longbridge.models.User;
+import longbridge.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Showboy on 27/03/2017.
+ */
+@Service
+public class UserService {
+    @Autowired
+    private UserRepo userRepository;
+
+    @PreAuthorize("hasAuthority('CREATE_USER')")
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+}
