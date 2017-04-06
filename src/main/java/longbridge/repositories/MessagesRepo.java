4b4fec198d580d67e06efa53b8b344eb01ffd5a1
Chigozirim Torti <nnasino2008@live.com>
2017-04-06 10:42:07 +0100

    rENAMe views to match current convention

--- src/main/java/longbridge/repositories/MessagesRepo.java
+++ src/main/java/longbridge/repositories/MessagesRepo.java
@@ -3,7 +3,6 @@ package longbridge.repositories;
 import longbridge.models.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sun.misc.resources.Messages;
 
 /**
  * Created by Wunmi on 27/03/2017.
