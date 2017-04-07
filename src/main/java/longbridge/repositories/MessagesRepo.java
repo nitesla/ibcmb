71cbbf62e73ac71a3c74f3b1113569124aaf6935
Oluwawunmi <soluwawunmi@gmail.com>
2017-04-07 09:37:01 +0100

    Message Service

--- src/main/java/longbridge/repositories/MessagesRepo.java
+++ src/main/java/longbridge/repositories/MessagesRepo.java
@@ -1,9 +1,9 @@
 package longbridge.repositories;
 
+import longbridge.models.MailBox;
 import longbridge.models.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.joda.time.LocalDateTime;
 import org.springframework.stereotype.Repository;
-import sun.misc.resources.Messages;
 
 /**
  * Created by Wunmi on 27/03/2017.
@@ -11,5 +11,13 @@ import sun.misc.resources.Messages;
 
 @Repository
 public interface MessagesRepo extends CommonRepo<Message, Long> {
+
+    Iterable<Message> getByMailBox(MailBox mailBox);
+
+    Iterable<Message> getByMailBoxAndSentTime(MailBox mailBox, LocalDateTime sentTime);
+
+    Iterable<Message> getByMailBoxAndSentTimeBetween(MailBox mailBox, LocalDateTime startTime, LocalDateTime endTime);
+
+    Iterable<Message> getBySentTimeBetween(LocalDateTime startTime, LocalDateTime endTime);
 
 }
