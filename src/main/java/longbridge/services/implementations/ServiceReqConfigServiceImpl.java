89ec84c9bb820015dad89e14c184b464da26217b
Oluwawunmi <soluwawunmi@gmail.com>
2017-04-10 12:55:45 +0100

    service request config dt

--- src/main/java/longbridge/services/implementations/ServiceReqConfigServiceImpl.java
+++ src/main/java/longbridge/services/implementations/ServiceReqConfigServiceImpl.java
@@ -1,18 +1,28 @@
 package longbridge.services.implementations;
 
 import longbridge.dtos.ServiceReqConfigDTO;
+import longbridge.repositories.ServiceReqConfigRepo;
 import longbridge.services.ServiceReqConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by Showboy on 08/04/2017.
  */
 @Service
 public class ServiceReqConfigServiceImpl implements ServiceReqConfigService {
+
+    @Autowired
+    private ServiceReqConfigRepo serviceReqConfigRepo;
+
     @Override
+    @Transactional
     public void addSeviceReqConfig(ServiceReqConfigDTO serviceReqConfig) {
 
-        System.out.println(serviceReqConfig.toString());
+        //this.serviceReqConfigRepo.save(serviceReqConfig);
+        //System.out.println(serviceReqConfig.toString());
     }
 
     @Override
