788cbdc9bc6ff86768b74c8a3a24e080786c7b4c
Yemi Dalley <Yemi Dalley@192.168.1.3>
2017-04-11 09:16:02 +0100

    Added Datatables lib

--- src/main/java/longbridge/controllers/admin/AdmServiceReqConfigController.java
+++ src/main/java/longbridge/controllers/admin/AdmServiceReqConfigController.java
@@ -6,6 +6,10 @@ import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
+import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -52,11 +56,17 @@ public class AdmServiceReqConfigController {
     }
 
     @GetMapping(path = "/all")
-    public @ResponseBody Iterable<ServiceReqConfigDTO> getConfigs(){
+    public @ResponseBody DataTablesOutput<ServiceReqConfigDTO> getConfigs(DataTablesInput input){
 
-        Iterable<ServiceReqConfigDTO> configList = serviceReqConfigService.getServiceReqConfigs();
-
-        return configList;
+    	int record = input.getStart();
+    	int max = input.getLength();
+        Page<ServiceReqConfigDTO> serviceReqConfigs = serviceReqConfigService.getServiceReqConfigs(new PageRequest(record/max, max));
+        DataTablesOutput<ServiceReqConfigDTO> out = new DataTablesOutput<ServiceReqConfigDTO>();
+        out.setDraw(input.getDraw());
+        out.setData(serviceReqConfigs.getContent());
+        out.setRecordsFiltered(serviceReqConfigs.getNumberOfElements());
+        out.setRecordsTotal(serviceReqConfigs.getTotalElements());
+        return out;
     }
 
     @GetMapping("/{reqId}/edit")
