3e0e1c466943b5df559a54d10312d0e9a32c993d
Oluwawunmi <soluwawunmi@gmail.com>
2017-04-12 09:41:26 +0100

    update

--- src/main/java/longbridge/controllers/retail/RetServiceRequestController.java
+++ src/main/java/longbridge/controllers/retail/RetServiceRequestController.java
@@ -1,6 +1,8 @@
 package longbridge.controllers.retail;
 
+import longbridge.dtos.CodeDTO;
 import longbridge.dtos.ServiceReqConfigDTO;
+import longbridge.services.CodeService;
 import longbridge.services.ServiceReqConfigService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,12 +23,16 @@ public class RetServiceRequestController {
     @Autowired
     private ServiceReqConfigService serviceReqConfigService;
 
+    @Autowired
+    private CodeService codeService;
+
     private Logger logger= LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/{reqId}")
     public String addConfig(@PathVariable Long reqId, Model model){
         ServiceReqConfigDTO serviceReqConfig = serviceReqConfigService.getServiceReqConfig(reqId);
-        logger.info("Request Details {}", serviceReqConfig);
+        Iterable<CodeDTO> fieldTypes = codeService.getCodesByType("SERVICE_REQUEST");
+        model.addAttribute("requestFieldType",fieldTypes);
         model.addAttribute("requestConfig", serviceReqConfig);
         return "cust/servicerequest/add";
     }
