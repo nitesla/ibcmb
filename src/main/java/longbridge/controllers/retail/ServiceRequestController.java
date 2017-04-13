971d3cfc6ddab7f4838899ecb313dc943f640331
Oluwawunmi <soluwawunmi@gmail.com>
2017-04-13 16:58:20 +0100

    service request controller

--- src/main/java/longbridge/controllers/retail/ServiceRequestController.java
+++ src/main/java/longbridge/controllers/retail/ServiceRequestController.java
@@ -8,15 +8,19 @@ import longbridge.services.CodeService;
 import longbridge.services.RequestService;
 import longbridge.services.ServiceReqConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Fortune on 4/5/2017.
  */
 
-@RestController
+@Controller
 @RequestMapping("retail/requests")
 public class ServiceRequestController {
 
@@ -49,6 +53,14 @@ public class ServiceRequestController {
                 //System.out.println(field.getTypeData());
                 //System.out.println(codeService.getCodesByType(field.getTypeData()));
                 field.setCodeDTOs(codeService.getCodesByType(field.getTypeData()));
+            }
+
+            if(field.getFieldType() != null && field.getFieldType().equals("List")){
+                String list = field.getTypeData();
+                String myList [] =list.split(",");
+                List<String> fieldList = Arrays.asList(myList);
+                System.out.println(fieldList);
+                model.addAttribute("fixedList", fieldList);
             }
         }
         //System.out.println(serviceReqConfig);
