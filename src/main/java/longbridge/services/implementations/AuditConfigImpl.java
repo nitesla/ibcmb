b31e5d00e3696fd078319ac20c9c34b9f4636522
Fortunatus Ekenachi <fcekenachi@gmail.com>
2017-04-20 19:24:32 +0100

    Fix Error handling

--- src/main/java/longbridge/services/implementations/AuditConfigImpl.java
+++ src/main/java/longbridge/services/implementations/AuditConfigImpl.java
@@ -2,17 +2,26 @@ package longbridge.services.implementations;
 
 import longbridge.models.AuditConfig;
 import longbridge.repositories.AuditConfigRepo;
+import longbridge.services.AuditConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by ayoade_farooq@yahoo.com on 4/19/2017.
  */
 @Service
-public class AuditConfigImpl {
+public class AuditConfigImpl implements AuditConfigService {
 
     @Autowired
     private AuditConfigRepo configRepo;
+    @Autowired
+	EntityManager entityManager;
 
 	public AuditConfig findEntity(String s) {
 		return configRepo.findFirstByEntityName(s);
