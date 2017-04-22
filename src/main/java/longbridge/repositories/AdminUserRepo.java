fadae3a8d248cfe1d3c5ca2cabb4564bab0cba38
Yemi Dalley <Yemi Dalley@192.168.216.1>
2017-04-22 17:07:46 +0100

    Fix admin: update last login time
    Signed-off-by: Yemi Dalley <Yemi Dalley@192.168.216.1>

--- src/main/java/longbridge/repositories/AdminUserRepo.java
+++ src/main/java/longbridge/repositories/AdminUserRepo.java
@@ -1,14 +1,11 @@
 package longbridge.repositories;
 
-import longbridge.dtos.AdminUserDTO;
 import longbridge.models.AdminUser;
-import longbridge.models.Permission;
-import longbridge.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.io.Serializable;
-import java.util.List;
 
 /**
  * Created by Wunmi on 27/03/2017.
@@ -17,4 +14,8 @@ import java.util.List;
 
 public interface AdminUserRepo extends CommonRepo<AdminUser, Long>{
     AdminUser findFirstByUserName(String s);
+   
+    @Modifying
+    @Query("update AdminUser u set u.lastLoginDate = current_timestamp() , u.lockedUntilDate = NULL, u.noOfLoginAttempts = 0 where u.userName = :name")
+    void updateUserAfterLogin(@Param("name") String userName);
 }
--- src/main/java/longbridge/security/adminuser/AdminAuthenticationSuccessHandler.java
+++ src/main/java/longbridge/security/adminuser/AdminAuthenticationSuccessHandler.java
@@ -12,6 +12,7 @@ import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -31,11 +32,13 @@ public class AdminAuthenticationSuccessHandler extends SavedRequestAwareAuthenti
 
 
     @Override
+    @Transactional
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         HttpSession session = request.getSession();
         if (session != null) {
         	session.setMaxInactiveInterval(30 * 60); //TODO this cannot be static
         } 
+        adminUserRepo.updateUserAfterLogin(authentication.getName());
         super.onAuthenticationSuccess(request, response, authentication);
     }
 
