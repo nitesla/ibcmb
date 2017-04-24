4f838cd0fe175b4ab712d23fc2a04c3b45a83797
Yemi Dalley <Yemi Dalley@192.168.216.1>
2017-04-24 20:47:17 +0100

    Added bootstrap notify
    Signed-off-by: Yemi Dalley <Yemi Dalley@192.168.216.1>

--- src/main/resources/static/bank/js/custom.js
+++ src/main/resources/static/bank/js/custom.js
@@ -5064,29 +5064,3 @@ if (typeof NProgress != 'undefined') {
 				
 	});
 
-
-$(document).ready(function() {
-    initnotices();
-    $("input.switch").bootstrapSwitch();
-});
-function initnotices(){
-    $("#fielderrors li").each(function(index, value) {
-        var tt = $(this).text();
-        $("[name='" + tt + "']").closest(".form-group").addClass("has-error");
-    });
-
-    $('.actionMessage li').each(function(index, value) {
-        var tt = $(this).text();
-        var notify = $.notify({message:tt}, { allow_dismiss: true ,type: 'success'});
-    });
-
-    $('.actionError li').each(function(index, value) {
-        var tt = $(this).text();
-        var notify = $.notify({message:tt}, { allow_dismiss: true ,type: 'danger'});
-    });
-
-    $('.fieldError li').each(function(index, value) {
-        var tt = $(this).text();
-        var notify = $.notify({message:tt}, { allow_dismiss: true ,type: 'danger'});
-    });
-}
