be034ea21da1727a36322c010fb3b590069c1495
Oluwawunmi <soluwawunmi@gmail.com>
2017-05-08 11:31:00 +0100

    update

---
+++ src/main/java/longbridge/forms/ConfirmCheque.java
@@ -0,0 +1,17 @@
+package longbridge.forms;
+
+/**
+ * Created by Showboy on 08/05/2017.
+ */
+public class ConfirmCheque {
+
+    private String amount;
+
+    private String wordAmount;
+
+    private String chequeNo;
+
+    private String beneficiary;
+
+    private String chequeDate;
+}
