520e02d38b01872c0f667d380093fc10088cfe44
Oluwawunmi <soluwawunmi@gmail.com>
2017-05-17 20:30:40 +0100

    Corporate Login

--- src/main/java/longbridge/dtos/DirectDebitDTO.java
+++ src/main/java/longbridge/dtos/DirectDebitDTO.java
@@ -8,72 +8,72 @@ import java.util.Date;
 
 public class DirectDebitDTO {
 
-		private Long id;
-	    private LocalBeneficiary beneficiary;
-	    private String debitAccount;
-	    private BigDecimal amount;
-	    private int intervalDays;
-	    private Date nextDebitDate;
-	    private Date dateCreated;
-	    private RetailUser retailUser;
-	    private String narration;
+    private Long id;
+    private LocalBeneficiary beneficiary;
+    private String debitAccount;
+    private BigDecimal amount;
+    private int intervalDays;
+    private Date nextDebitDate;
+    private Date dateCreated;
+    private RetailUser retailUser;
+    private String narration;
 
 
-	    public Long getId() {
-	        return id;
-	    }
+    public Long getId() {
+        return id;
+    }
 
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-		public LocalBeneficiary getBeneficiary() {
-			return beneficiary;
-		}
+    public LocalBeneficiary getBeneficiary() {
+        return beneficiary;
+    }
 
-		public void setBeneficiary(LocalBeneficiary beneficiary) {
-			this.beneficiary = beneficiary;
-		}
+    public void setBeneficiary(LocalBeneficiary beneficiary) {
+        this.beneficiary = beneficiary;
+    }
 
-		public BigDecimal getAmount() {
-			return amount;
-		}
+    public BigDecimal getAmount() {
+        return amount;
+    }
 
-		public void setAmount(BigDecimal amount) {
-			this.amount = amount;
-		}
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 
-		public int getIntervalDays() {
-			return intervalDays;
-		}
+    public int getIntervalDays() {
+        return intervalDays;
+    }
 
-		public void setIntervalDays(int intervalDays) {
-			this.intervalDays = intervalDays;
-		}
+    public void setIntervalDays(int intervalDays) {
+        this.intervalDays = intervalDays;
+    }
 
-		public Date getNextDebitDate() {
-			return nextDebitDate;
-		}
+    public Date getNextDebitDate() {
+        return nextDebitDate;
+    }
 
-		public void setNextDebitDate(Date nextDebitDate) {
-			this.nextDebitDate = nextDebitDate;
-		}
+    public void setNextDebitDate(Date nextDebitDate) {
+        this.nextDebitDate = nextDebitDate;
+    }
 
-		public Date getDateCreated() {
-			return dateCreated;
-		}
+    public Date getDateCreated() {
+        return dateCreated;
+    }
 
-		public void setDateCreated(Date dateCreated) {
-			this.dateCreated = dateCreated;
-		}
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
 
-		public RetailUser getRetailUser() {
-			return retailUser;
-		}
+    public RetailUser getRetailUser() {
+        return retailUser;
+    }
 
-		public void setRetailUser(RetailUser retailUser) {
-			this.retailUser = retailUser;
-		}
+    public void setRetailUser(RetailUser retailUser) {
+        this.retailUser = retailUser;
+    }
 
 	public String getDebitAccount() {
 		return debitAccount;
