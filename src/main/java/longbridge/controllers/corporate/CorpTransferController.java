a09a42774b6ba95ce31faf32709ae002afa5931b
Sylvester Ofueu <sylvester.ofueu@longbridgetech.com>
2017-05-26 20:09:22 +0100

    Authorize Transfer

--- src/main/java/longbridge/controllers/corporate/CorpTransferController.java
+++ src/main/java/longbridge/controllers/corporate/CorpTransferController.java
@@ -2,14 +2,18 @@ package longbridge.controllers.corporate;
 
 import longbridge.dtos.CorporateDTO;
 import longbridge.dtos.CorporateUserDTO;
+import longbridge.exception.InternetBankingException;
 import longbridge.models.CorpTransferRequest;
 import longbridge.models.Corporate;
 import longbridge.models.CorporateUser;
 import longbridge.models.PendingAuthorization;
 import longbridge.repositories.CorpTransferRequestRepo;
 import longbridge.repositories.CorporateRepo;
+import longbridge.services.CorpTransferService;
 import longbridge.services.CorporateService;
 import longbridge.services.CorporateUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
 import java.security.Principal;
@@ -40,24 +45,17 @@ public class CorpTransferController {
     @Autowired
     private CorporateUserService corporateUserService;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+
     @Autowired
     CorpTransferRequestRepo transferRequestRepo;
+    @Autowired private CorpTransferService corpTransferService;
 
     @GetMapping("/{id}/{amount}")
     public void getQualifiedAuthorizers(@PathVariable Long id, @PathVariable String amount) {
-        CorpTransferRequest transferRequest = new CorpTransferRequest();
-        transferRequest.setAmount(new BigDecimal(amount));
-        Corporate corporate = corporateRepo.findOne(id);
-        transferRequest.setCorporate(corporate);
-        List<CorporateUser> authorizers = corporateService.getQualifiedAuthorizers(transferRequest);
-        PendingAuthorization pendingAuthorization = new PendingAuthorization();
-        List<PendingAuthorization> pendingAuthorizations = new ArrayList<>();
-        for (CorporateUser authorizer : authorizers) {
-            pendingAuthorization.setAuthorizer(authorizer);
-            pendingAuthorizations.add(pendingAuthorization);
-        }
-        transferRequest.setPendingAuthorizations(pendingAuthorizations);
-        transferRequestRepo.save(transferRequest);
+
     }
 
     @GetMapping("/pending")
@@ -66,6 +64,20 @@ public class CorpTransferController {
         CorporateUser corporateUser = corporateUserService.getUserByName(principal.getName());
         List<PendingAuthorization> pendingAuthorizations=corporateUser.getPendingAuthorizations();
         model.addAttribute("pendingAuthorizations",pendingAuthorizations);
+        return "corp/transfer/pendingtransfer/view";
+    }
+    @GetMapping("/{id}/authorize")
+    public String authorizeTransfer(@PathVariable Long id, Principal principal, Model model, RedirectAttributes redirectAttributes){
+try {
+    CorporateUser corporateUser = corporateUserService.getUserByName(principal.getName());
+    String message = corpTransferService.authorizeTransfer(corporateUser, id);
+    redirectAttributes.addFlashAttribute("message", message);
+}
+catch (InternetBankingException e){
+    logger.error("AN ERROR HAS OCCURRED",e);
+    redirectAttributes.addFlashAttribute("failure", e.getMessage());
+
+}
         return "corp/transfer/pendingtransfer/view";
     }
 }
