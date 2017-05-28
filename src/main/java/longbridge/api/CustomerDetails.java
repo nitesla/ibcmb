8be1a11bf76e1be2d1638c507b3608799bafe034
Fortunatus Ekenachi <fcekenachi@gmail.com>
2017-05-28 19:09:26 +0100

    Application update

--- src/main/java/longbridge/api/CustomerDetails.java
+++ src/main/java/longbridge/api/CustomerDetails.java
@@ -54,14 +54,6 @@ public class CustomerDetails {
         return dateOfBirth;
     }
 
-    public String getBvn() {
-        return bvn;
-    }
-
-    public void setBvn(String bvn) {
-        this.bvn = bvn;
-    }
-
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
