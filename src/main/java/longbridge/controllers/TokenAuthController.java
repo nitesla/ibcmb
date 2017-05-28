87da7d1ceeedbd6eff837f00adff26a298d07c7a
Fortunatus Ekenachi <fcekenachi@gmail.com>
2017-05-29 00:15:16 +0100

    Admin & Ops Token Authentication

--- src/main/java/longbridge/controllers/TokenAuthController.java
+++ src/main/java/longbridge/controllers/TokenAuthController.java
@@ -2,23 +2,45 @@ package longbridge.controllers;
 
 import longbridge.services.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AfterDomainEventPublication;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
 /**
  * Created by Fortune on 5/26/2017.
  */
-@Controller
+
+@RequestMapping("/token")
 public class TokenAuthController {
 
     @Autowired
     private SecurityService securityService;
 
-    @GetMapping("/authenticate")
+    @PostMapping("/authenticate")
     public String authenticate(HttpSession session){
 
-        return "";
+        //TODO authenticate
+        String url = "";
+        if(session.getAttribute("url")!=null) {
+            url = (String) session.getAttribute("url");
+        }
+
+        return "redirect:"+url;
+    }
+
+    @GetMapping("/admin")
+    public String getAdminToken(){
+        return "adm/admin/token";
+    }
+
+
+    @GetMapping("/ops")
+    public String getOpsToken(){
+        return "ops/token";
     }
 }
---
+++ src/main/java/longbridge/controllers/admin/AdmTokenController.java
@@ -0,0 +1,11 @@
+package longbridge.controllers.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Fortune on 5/28/2017.
+ */
+
+@RequestMapping("/admin/token")
+public class AdmTokenController {
+}
