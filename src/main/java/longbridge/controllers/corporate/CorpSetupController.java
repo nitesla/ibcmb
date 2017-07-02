41484acc0ba6b970bab7127a590489ce8607dde2
Oluwawunmi <soluwawunmi@gmail.com>
2017-07-02 14:10:39 +0100

    Token Username

--- src/main/java/longbridge/controllers/corporate/CorpSetupController.java
+++ src/main/java/longbridge/controllers/corporate/CorpSetupController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -19,5 +20,9 @@ public class CorpSetupController {
     @Autowired
     private MessageService messageService;
 
+    @GetMapping
+    public String setUp(){
+        return "corp/setup/sec";
+    }
 
 }
