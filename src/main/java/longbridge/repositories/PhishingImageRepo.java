bcd95d59837205aed11f8da0e065e129c66736da
Oluwawunmi <soluwawunmi@gmail.com>
2017-07-03 17:18:43 +0100

    Token Username

--- src/main/java/longbridge/repositories/PhishingImageRepo.java
+++ src/main/java/longbridge/repositories/PhishingImageRepo.java
@@ -1,7 +1,10 @@
 package longbridge.repositories;
 
 import longbridge.models.PhishingImage;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by Longbridge on 22/06/2017.
@@ -9,4 +12,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PhishingImageRepo extends CommonRepo<PhishingImage, Long>{
 
+    @Query(nativeQuery=true, value="SELECT imagePath  FROM question ORDER BY random() LIMIT 10")
+    List<PhishingImage> findImagePath();
 }
