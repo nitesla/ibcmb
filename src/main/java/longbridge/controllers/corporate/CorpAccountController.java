7c9158cfaed5cc4036cdb8a1378f5ce914749c3d
Farooq Ayoade <farooq.ayoade@longbridgetech.com>
2017-07-05 10:20:12 +0100

    Corporate Transfer rules

--- src/main/java/longbridge/controllers/corporate/CorpAccountController.java
+++ src/main/java/longbridge/controllers/corporate/CorpAccountController.java
@@ -116,7 +116,7 @@ public class CorpAccountController {
     @PostMapping("/customize")
     public String updateCustom(@Valid CustomizeAccount customizeAccount, BindingResult result,RedirectAttributes redirectAttributes, Model model)throws Exception{
         if (result.hasErrors()){
-            model.addAttribute("message","Pls correct the errors");
+            model.addAttribute("message","Name field cannot be empty");
             return "corp/account/customize";
         }
 try {
