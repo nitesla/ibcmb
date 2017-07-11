6f88a7cde967024da3e699cd731af7545ffb4dc9
Oluwawunmi <soluwawunmi@gmail.com>
2017-07-11 14:26:11 +0100

    ret-layout

--- src/main/java/longbridge/controllers/MainController.java
+++ src/main/java/longbridge/controllers/MainController.java
@@ -56,6 +56,11 @@ public class MainController {
     @Autowired
     private  FaqsService faqsService;
 
+    @GetMapping("/testing")
+    public String testing(){
+        return "abcd";
+    }
+
 
     @RequestMapping(value = {"/", "/home"})
     public String getHomePage(@RequestParam Optional<HttpServletRequest> request) {
