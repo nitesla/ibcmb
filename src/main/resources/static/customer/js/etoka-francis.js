a9097a3dc8a3e5b050afb5a439020ab60aae2461
phran6 <oghenerie.etoka@longbridge.com>
2017-07-11 14:24:51 +0100

    chg

---
+++ src/main/resources/static/customer/js/etoka-francis.js
@@ -0,0 +1,14 @@
+function currentBar (arg)
+{
+    var barTitleHolder = document.getElementById('bar');
+
+    barTitleHolder.innerHTML = arg;
+}
+
+
+function breadCrumb (arg)
+{
+    var bcHolder = document.getElementById('bread-crumb');
+
+    bcHolder.innerHTML = arg;
+}
