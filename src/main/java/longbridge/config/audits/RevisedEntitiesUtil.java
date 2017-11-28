4755ea561fb18d692c36790798e62fb6c5e12aa4
Shina <repo_integration@longbridgetech.com>
2017-11-28 16:40:36 +0100

    tidy up audit

--- src/main/java/longbridge/config/audits/RevisedEntitiesUtil.java
+++ src/main/java/longbridge/config/audits/RevisedEntitiesUtil.java
@@ -195,7 +195,6 @@ public class RevisedEntitiesUtil {
             SqlParameterSource namedParameters = new MapSqlParameterSource("username", username);
             List<Map<String ,Object>> entityDetails = namedParameterJdbcTemplate.queryForList(sql, namedParameters);
 if(entityDetails != null) {
-    logger.info("the entityDetails {}",entityDetails.get(0));
     return entityDetails.get(0).get("ID").toString();
 }else {
     return "";
