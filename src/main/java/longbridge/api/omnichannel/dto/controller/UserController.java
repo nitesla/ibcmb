2c70c82c618f992c32f47b5003eff2a7c2cd308d
Fortunatus Ekenachi <fcekenachi@gmail.com>
2017-12-18 18:12:31 +0100

    Added Coverage report fpr retail test

--- src/main/java/longbridge/api/omnichannel/dto/controller/UserController.java
+++ src/main/java/longbridge/api/omnichannel/dto/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
