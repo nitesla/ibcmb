44953c548700a60259d218e4b269c02a6db8a275
Fortunatus Ekenachi <fcekenachi@gmail.com>
2017-12-18 17:30:36 +0100

    Added Omnichannel API to get customer Info
    (cherry picked from commit 66287e5)

--- src/main/java/longbridge/services/RetailUserService.java
+++ src/main/java/longbridge/services/RetailUserService.java
@@ -1,6 +1,8 @@
 package longbridge.services;
 
 import longbridge.api.CustomerDetails;
+import longbridge.api.omnichannel.dto.CustomerInfo;
+import longbridge.api.omnichannel.dto.RetailUserCredentials;
 import longbridge.dtos.AccountDTO;
 import longbridge.dtos.RetailUserDTO;
 import longbridge.exception.InternetBankingException;
@@ -151,5 +153,6 @@ public interface RetailUserService {
     void increaseNoOfTokenAttempt(RetailUser user);
     void resetNoOfTokenAttempt(RetailUser user);
     String validateAccount();
-    
+
+    CustomerInfo getCustomerInfo(RetailUserCredentials userCredentials);
 }
