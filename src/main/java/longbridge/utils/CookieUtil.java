42ca1b897560bc735a3b785454602fe3c901a15b
Shina <repo_integration@longbridgetech.com>
2018-01-11 15:02:36 +0100

    session working for retail and corporate

---
+++ src/main/java/longbridge/utils/CookieUtil.java
@@ -0,0 +1,28 @@
+package longbridge.utils;
+
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Longbridge on 1/11/2018.
+ */
+public final class CookieUtil {
+
+        private CookieUtil() {}
+
+        public static Cookie getCookie(HttpServletRequest request) {
+            if (request.getCookies() != null) {
+                for (Cookie cookie : request.getCookies()) {
+                    if (cookie.getName().equalsIgnoreCase("time_out_time")) {
+                        System.out.println("cookie match");
+                        return cookie;
+                    }
+                }
+            }
+            Cookie cookie = new Cookie("time_out_time","");
+            cookie.setPath("/");
+            return cookie;
+        }
+
+}
