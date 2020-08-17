3da85adc6482e9db76152bdc24ab4907c18e81e1
Kolapo Bukola Rukayat <rukayat.kolapo@longbridgetech.com>
2020-08-17 13:51:15 +0100

    Retail modify link

--- src/main/java/longbridge/controllers/corporate/CorpOwnTransferController.java
+++ src/main/java/longbridge/controllers/corporate/CorpOwnTransferController.java
@@ -149,7 +149,7 @@ public class CorpOwnTransferController {
 
 
     @PostMapping("/edit")
-    public String editTransfer(@ModelAttribute("corpTransferRequest") CorpTransferRequestDTO transferRequestDTO, Model model, HttpServletRequest request) {
+    public String editTransfer(@ModelAttribute("corpTransferRequest") TransferRequestDTO transferRequestDTO, Model model, HttpServletRequest request) {
         transferRequestDTO.setTransferType(TransferType.OWN_ACCOUNT_TRANSFER);
         transferRequestDTO.setFinancialInstitution(financialInstitutionService.getFinancialInstitutionByCode(bankCode));
         model.addAttribute("corpTransferRequest", transferRequestDTO);
