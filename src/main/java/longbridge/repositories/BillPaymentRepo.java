3b9c13517f7b599498fb34a00f8650c38c05b310
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2020-10-15 03:12:14 +0000

    Merge branch 'cmb_final_dev' of https://gitlab.com/longbridgesoftwaredev/ibcmb into cmb_tesla
    Signed-off-by: Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
    
    # Conflicts:
    #	velocity.log

--- src/main/java/longbridge/repositories/BillPaymentRepo.java
+++ src/main/java/longbridge/repositories/BillPaymentRepo.java
@@ -15,7 +15,9 @@ public interface BillPaymentRepo extends CommonRepo<BillPayment,Long> {
 
     Page<BillPayment> findByRequestReferenceAndStatusNotNullOrderByCreatedOnDesc(String rn, Pageable pageable);
 
+    Page<BillPayment> findByCustomerIdAndStatusNotNullOrderByCreatedOnDesc(String ci, Pageable pageable);
 
-    @Query("select r from BillPayment r where r.requestReference=:requestReference and (upper(r.billerName) like %:search% or concat(r.amount,'')  like %:search% or upper(r.status)  like %:search% or upper(r.paymentItemName)  like %:search% or upper(r.categoryName)  like %:search% or upper(r.createdOn) like %:search%)order by r.createdOn desc")
-    Page<BillPayment> findUsingPattern(@Param("requestReference") String requestReference, @Param("search") String search, Pageable pageable);
+
+    @Query("select r from BillPayment r where r.customerId=:customerId and (upper(r.billerName) like %:search% or concat(r.amount,'')  like %:search% or upper(r.status)  like %:search% or upper(r.paymentItemName)  like %:search% or upper(r.categoryName)  like %:search% or upper(r.createdOn) like %:search%)order by r.createdOn desc")
+    Page<BillPayment> findUsingPattern(@Param("customerId") String customerId, @Param("search") String search, Pageable pageable);
 }
