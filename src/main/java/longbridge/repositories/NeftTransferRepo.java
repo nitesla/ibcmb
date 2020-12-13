f73b6bafa756f442edf78bc2783b82812622c4e2
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2020-12-13 15:33:21 +0100

    Merge branch 'cmb_final_dev' of https://gitlab.com/longbridgesoftwaredev/ibcmb into cmb_tesla
    Signed-off-by: Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
    
    # Conflicts:
    #	src/main/java/longbridge/controllers/corporate/CorpNEFTTransferController.java
    #	src/main/java/longbridge/controllers/corporate/CorpTransferController.java
    #	src/main/java/longbridge/services/implementations/CorpTransferServiceImpl.java

--- src/main/java/longbridge/repositories/NeftTransferRepo.java
+++ src/main/java/longbridge/repositories/NeftTransferRepo.java
@@ -6,6 +6,7 @@ import longbridge.models.RetailUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,8 +22,8 @@ public interface NeftTransferRepo extends CommonRepo<NeftTransfer, Long>{
 
 //    @Query(value = "select n from NeftTransfer n where n.SettlementTime = 'not settled' and n.retailUser =:user ")
     @Query(value = "select n from NeftTransfer n where n.retailUser =:user ")
-    Page<NeftTransfer> findByRetailUser(RetailUser user, Pageable pageable);
+    Page<NeftTransfer> findByRetailUser(@Param("user") RetailUser user, Pageable pageable);
 
     @Query(value = "select n from NeftTransfer n where n.corporate =:corporate ")
-    Page<NeftTransfer> findByCorporate(Corporate corporate, Pageable pageable);
+    Page<NeftTransfer> findByCorporate(@Param("corporate") Corporate corporate, Pageable pageable);
 }
