23a1b72010c71ec63a07c0e9a0513e3f8cc89c5a
Habib <habib.muhammad@longbridgetech.com>
2021-01-29 09:51:54 +0100

    Auto loading Neft Banks

--- src/main/java/longbridge/repositories/NeftBankRepo.java
+++ src/main/java/longbridge/repositories/NeftBankRepo.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface NeftBankRepo extends CommonRepo<NeftBank, Long> {
     List<NeftBank> findAllByBankName(String bankName);
     Page<NeftBank> findAllByBankName(String bankName, Pageable pageable);
+    NeftBank findByBankNameAndBranchNameAndSortCode(String bankName, String branchName, String sortCode);
 }
