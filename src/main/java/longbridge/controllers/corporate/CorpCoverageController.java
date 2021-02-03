cf28dca0d18eac4aa3e30bf4e5b4bdf660925831
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2021-02-03 12:09:10 +0100

    Merge branch 'cmb_final_dev' of https://gitlab.com/longbridgesoftwaredev/ibcmb into cmb_final_dev
    Signed-off-by: Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
    
    # Conflicts:
    #	src/main/resources/static/customer/gtl/css/custom.css

--- src/main/java/longbridge/controllers/corporate/CorpCoverageController.java
+++ src/main/java/longbridge/controllers/corporate/CorpCoverageController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/longbridge/controllers/retail/CoverageController.java
+++ src/main/java/longbridge/controllers/retail/CoverageController.java
@@ -1,17 +1,11 @@
 package longbridge.controllers.retail;
 
 
-import longbridge.dtos.CoverageDetailsDTO;
 import longbridge.services.CoverageService;
 import longbridge.services.RetailUserService;
-import longbridge.utils.DataTablesUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
-import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.security.Principal;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/retail/coverage")
